ae1eac01dcd736708be46acfb277529022ebffab
Stalbar <aleksey.smorshyok@gmail.com>
2024-02-23 16:22:37 +0300

    implement owners and positions stores

---
+++ GarageAdministration.WPF/Commons/Stores/OwnersStore.cs
@@ -0,0 +1,52 @@
+﻿using GarageAdministration.Domain.Commands;
+using GarageAdministration.Domain.Models;
+
+namespace GarageAdministration.WPF.Commons.Stores;
+
+public class OwnersStore
+{
+    private readonly List<Owner> _owners;
+    private readonly ICreateCommand<Owner> _createCommand;
+    private readonly IUpdateCommand<Owner> _updateCommand;
+    private readonly IDeleteCommand _deleteCommand;
+
+    public IEnumerable<Owner> Owners => _owners;
+
+    public event Action<Owner>? OwnerAdded; 
+    public event Action<Owner>? OwnerUpdated;
+    public event Action<int>? OwnerDeleted;
+
+    public OwnersStore(ICreateCommand<Owner> createCommand, IUpdateCommand<Owner> updateCommand,
+        IDeleteCommand deleteCommand)
+    {
+        _owners = new();
+        _createCommand = createCommand;
+        _updateCommand = updateCommand;
+        _deleteCommand = deleteCommand;
+    }
+
+    public async Task Add(Owner owner)
+    {
+        await _createCommand.Execute(owner);
+        _owners.Add(owner);
+        OwnerAdded?.Invoke(owner);
+    }
+
+    public async Task Update(Owner owner)
+    {
+        await _updateCommand.Execute(owner);
+        var index = _owners.FindIndex(o => o.Id == owner.Id);
+        if (index != -1)
+        {
+            _owners[index] = owner;
+        }
+        OwnerUpdated?.Invoke(owner);
+    }
+
+    public async Task Delete(int id)
+    {
+        await _deleteCommand.Execute(id);
+        _owners.RemoveAll(o => o.Id == id);
+        OwnerDeleted?.Invoke(id);
+    }
+}
---
+++ GarageAdministration.WPF/Commons/Stores/PositionsStore.cs
@@ -0,0 +1,51 @@
+﻿using GarageAdministration.Domain.Commands;
+using GarageAdministration.Domain.Models;
+
+namespace GarageAdministration.WPF.Commons.Stores;
+
+public class PositionsStore
+{
+    private readonly List<Position> _positions;
+    private readonly ICreateCommand<Position> _createCommand;
+    private readonly IUpdateCommand<Position> _updateCommand;
+    private readonly IDeleteCommand _deleteCommand;
+
+    public IEnumerable<Position> Positions => _positions;
+
+    public event Action<Position>? PositionAdded;
+    public event Action<Position>? PositionUpdated;
+    public event Action<int>? PositionDeleted;
+
+    public PositionsStore(ICreateCommand<Position> createCommand, IUpdateCommand<Position> updateCommand, IDeleteCommand deleteCommand)
+    {
+        _positions = new();
+        _createCommand = createCommand;
+        _updateCommand = updateCommand;
+        _deleteCommand = deleteCommand;
+    }
+
+    public async Task Add(Position position)
+    {
+        await _createCommand.Execute(position);
+        _positions.Add(position);
+        PositionAdded?.Invoke(position);
+    }
+
+    public async Task Update(Position position)
+    {
+        await _updateCommand.Execute(position);
+        var index = _positions.FindIndex(p => p.Id == position.Id);
+        if (index != -1)
+        {
+            _positions[index] = position;
+        }
+        PositionUpdated?.Invoke(position);
+    }
+
+    public async Task Delete(int id)
+    {
+        await _deleteCommand.Execute(id);
+        _positions.RemoveAll(p => p.Id == id);
+        PositionDeleted?.Invoke(id);
+    }
+}
