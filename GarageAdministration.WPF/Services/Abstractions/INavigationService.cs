92ed44cde88fa596e37e3cd163c2d6b01b2720d5
Stalbar <aleksey.smorshyok@gmail.com>
2024-04-03 22:56:26 +0300

    add stores

---
+++ GarageAdministration.WPF/Services/Abstractions/INavigationService.cs
@@ -0,0 +1,9 @@
+﻿using GarageAdministration.WPF.Commons;
+
+namespace GarageAdministration.WPF.Services.Abstractions;
+
+public interface INavigationService
+{
+    ViewModelBase CurrentView { get; }
+    void NavigateTo<T>() where T : ViewModelBase;
+}
---
+++ GarageAdministration.WPF/Services/Implementations/NavigationService.cs
@@ -0,0 +1,29 @@
+﻿using GarageAdministration.WPF.Commons;
+using GarageAdministration.WPF.Services.Abstractions;
+
+public class NavigationService : ViewModelBase, INavigationService
+{
+    private readonly Func<Type, ViewModelBase> _viewModelFactory;
+    private ViewModelBase _currentView;
+
+    public ViewModelBase CurrentView
+    {
+        get => _currentView;
+        private set
+        {
+            _currentView = value;
+            OnPropertyChanged(nameof(CurrentView));
+        }
+    }
+
+    public NavigationService(Func<Type, ViewModelBase> viewModelFactory)
+    {
+        _viewModelFactory = viewModelFactory;
+    }
+    
+    public void NavigateTo<TViewModel>() where TViewModel : ViewModelBase
+    {
+        ViewModelBase viewModel = _viewModelFactory.Invoke(typeof(TViewModel));
+        CurrentView = viewModel;
+    }
+}
