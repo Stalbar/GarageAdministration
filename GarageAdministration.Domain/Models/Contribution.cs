aab948ac7d533b63adc1ef8cd54ad01ba306f824
Stalbar <aleksey.smorshyok@gmail.com>
2024-05-07 09:30:29 +0300

    add contribution entity

---
+++ GarageAdministration.Domain/Models/Contribution.cs
@@ -0,0 +1,17 @@
+﻿namespace GarageAdministration.Domain.Models;
+
+public class Contribution
+{
+    public int Id { get; }
+    public decimal MembershipFee { get; }
+    public decimal WaterFee { get; }
+    public Garage Garage { get; }
+
+    public Contribution(int id, decimal membershipFee, decimal waterFee, Garage garage)
+    {
+        Id = id;
+        MembershipFee = membershipFee;
+        WaterFee = waterFee;
+        Garage = garage;
+    }
+}
---
+++ GarageAdministration.EF/DTOs/ContributionDto.cs
@@ -0,0 +1,10 @@
+﻿namespace GarageAdministration.EF.DTOs;
+
+public class ContributionDto
+{
+    public int Id { get; set; }
+    public decimal MembershipFee { get; }
+    public decimal WaterFee { get; }
+    public int GarageId { get; set; }
+    public GarageDto? Garage { get; set; }
+}
